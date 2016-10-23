f6b9878fdd7c3d81cdd133e36251647b17e79074
Mark Stacy <mbstacy@gmail.com>
2016-10-23 16:14:17 -0500

    syntax error on append, missing )

--- api.js
+++ api.js
@@ -28,7 +28,7 @@ $(function() {
 function load_example_task(){
     addtask_template = Handlebars.templates['tmpl-add-task']
     $('#home').empty()
-    $('#home').append(addtask_template({"x":0,"y":0})
+    $('#home').append(addtask_template({"x":0,"y":0}))
 
     $('#addTask').click(function(){run_example_task();})
 }
