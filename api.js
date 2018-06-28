545bfa3a916ff56db48ce5d2c01ae540b8a3877d
Mark Stacy <mbstacy@gmail.com>
2018-06-28 16:25:37 -0600

    remove files after upload

--- api.js
+++ api.js
@@ -30,7 +30,8 @@ function load_example_task(){
     $('#home').empty()
     $('#home').append(addtask_template({"x":10,"y":10,"csrftoken":getCookie('csrftoken')}))
     //Remove files after successfull upload
-    var myDropzone = $('.dropzone')[0].dropzone
+    var myDropzone = $('.dropzone')[0].dropzone;
+    console.log(myDropzone);
     myDropzone.on("complete", function(file) {
         myDropzone.removeFile(file);
     });
