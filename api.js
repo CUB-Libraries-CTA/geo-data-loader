38103cfe4d54edba8b033bf197fb4ed01d063b8a
Mark Stacy <mbstacy@gmail.com>
2019-03-06 17:39:34 -0700

    update mymodal

--- api.js
+++ api.js
@@ -754,7 +754,7 @@ function general_status(data, html_result) {
     $("#getblight").click(function() {
       serilize_formdata("geoblacklightform");
     });
-    $("#modalBody").hide();
+    $("#myModal").hide();
   }
   if (data.hasOwnProperty("task_name")) {
     if (
